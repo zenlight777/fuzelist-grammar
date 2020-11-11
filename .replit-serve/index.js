6d9ff3e36b0bec4c7bfe93006a2aee4a77255b90
dev4light <dev4light@gmail.com>
2020-11-11 15:33:12 +0000

    update server

--- .replit-serve/index.js
+++ .replit-serve/index.js
@@ -5,7 +5,17 @@ const app = express()
 const DIST = path.resolve(process.cwd() + '/../dist/')
 console.log(DIST)
 
-app.use('/', express.static(DIST));
+app.use('/dist', express.static(DIST));
+
+app.use('/', (req, res) => {
+  var url = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+  res.set('Content-Type', 'text/plain');
+  let r = 'Deploy URL: ' + url + 'dist';
+  r += '\n'
+  r += 'Repl.it URL: ' + url
+  res.send(r);
+});
 
 app.listen(3000, function() {
   console.log('listening');
